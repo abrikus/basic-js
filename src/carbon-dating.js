d2b724f7ee53087619ddbe793af556eb4bfa2c13
abriku <oaoandrew@gmail.com>
2020-02-21 22:13:29 +0300

    feat: carbon solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sampleActivityFloat = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string') return false;
+  if (sampleActivityFloat < MODERN_ACTIVITY && sampleActivityFloat > 0) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityFloat) * HALF_LIFE_PERIOD / 0.693);
+  }
+  return false;
 };
