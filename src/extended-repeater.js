c7c7dbf20a75e712bec4b3db72ce54a8c59f671e
abriku <oaoandrew@gmail.com>
2020-03-22 20:20:20 +0300

    feat: add repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,40 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let separator = '+';
+    let repeatTimes = 0;
+    let addition = '';
+    let additionRepeatTimes = 0;
+    let additionSeparator = '';
+    let additionStr = '';
+
+    if (options.hasOwnProperty('repeatTimes')) {
+        repeatTimes = options['repeatTimes'];
+    }
+
+    if (options.hasOwnProperty('separator')) {
+        separator = options['separator'];
+    }
+
+    if (options.hasOwnProperty('addition')) {
+        addition = options['addition'];
+
+        if (addition === null) {
+            addition = 'null';
+        }
+    }
+
+    if (options.hasOwnProperty('additionRepeatTimes')) {
+        additionRepeatTimes = options['additionRepeatTimes'];
+    }
+
+    if (options.hasOwnProperty('additionSeparator')) {
+        additionSeparator = options['additionSeparator'];
+    }
+
+    let mainArr = new Array (repeatTimes);
+    let additionArr = new Array (additionRepeatTimes);
+
+    additionStr = additionArr.fill(addition).join(additionSeparator);
+    str += additionStr;
+    return mainArr.fill(str).join(separator);
 };
   
