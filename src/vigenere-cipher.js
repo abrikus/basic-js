bbc425238b772387971fb2997d8e5e77751bbe3a
abriku <oaoandrew@gmail.com>
2020-03-22 19:06:15 +0300

    feat: add vigenere solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,78 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor (isDirect) {
+        this.isDirect = isDirect;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(input, key) {
+        if (!input || !key || input.length <= 0 || key.length <= 0) {
+            throw new Error();
+        }
+
+        input = input.toUpperCase();
+        key = key.toUpperCase();
+
+        let res = [];
+
+        for (let i = 0, j = 0; i < input.length; i++) {
+            if (!input[i].match(/[A-Z]/)) {
+                res.push(input[i]);
+                continue;
+            }
+            
+            let inputCharCode = input[i].charCodeAt(0);
+            let keyCharCode = key[j].charCodeAt(0);
+            j++;
+
+            if (j >= key.length) {
+                j = 0;
+            }
+
+            let encrypted = String.fromCharCode((inputCharCode + keyCharCode) % 26 + 65);
+
+            res.push(encrypted);
+        }
+        console.log(res);
+        if (this.isDirect) {
+            res.reverse();
+        }
+
+        return res.join('');
+    }
+
+    decrypt(encryptedWord, key) {
+        if (!encryptedWord || !key || encryptedWord.length <= 0 || key.length <= 0) {
+            throw new Error();
+        }
+
+        encryptedWord = encryptedWord.toUpperCase();
+        key = key.toUpperCase();
+
+        let res = [];
+
+        for (let i = 0, j = 0; i < encryptedWord.length; i++) {
+            if (!encryptedWord[i].match(/[A-Z]/)) {
+                res.push(encryptedWord[i]);
+                continue;
+            }
+
+            let encryptedCharCode = encryptedWord[i].charCodeAt();
+            let keyCharCode = key[j].charCodeAt();
+            j++;
+
+            if (j >= key.length) {
+                j = 0;
+            }
+
+            let decrypted = String.fromCharCode((encryptedCharCode + 26 - keyCharCode) % 26 + 65);
+
+            res.push(decrypted);
+        }
+
+        if (this.isDirect) {
+            res.reverse();
+        }
+
+        return res.join('');
     }
 }
 
